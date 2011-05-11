7c46b8339de65731654c8d3fd677bfe5a66eb28f
Nick Payne <nick@kurai.co.uk>
2011-05-11 22:52:42 +0100

    tinkering with jade index template

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,24 @@
 module.exports = function(app) {
+    /**
+     * Home Page
+     */
     app.get('/', function(req, res) {
-        res.render('index');
+        res.render('index', {
+            authed: false
+        });
+    });
+
+    /**
+     * Auth endpoint (ping off to twitter)
+     */
+    app.get('/auth', function(req, res) {
+        // redirect off to twitter
+    });
+
+    /**
+     * Authed endpoint (return from twitter)
+     */
+    app.get('/authed', function(req, res) {
+        // interpret twitter response, do stuff
     });
 }
